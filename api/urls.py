58970f7487b1b8cf0b107b46b922ebe01bd1ee1a
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-04-26 01:41:45 +0600

    SSLCommerz payment gateway initiated

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter, DefaultRouter
 from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet, ProductImageViewSet
-from order.views import CartViewSet, CartItemViewSet, OrderViewSet
+from order.views import CartViewSet, CartItemViewSet, OrderViewSet, initiate_payment
 from rest_framework_nested import routers
 
 router= routers.DefaultRouter()
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include(cart_router.urls)),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
+    path("payment/initiate/", initiate_payment, name="initiate-payment"),
 ]
 
 
--- order/views.py
+++ order/views.py
@@ -7,9 +7,11 @@ from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.exceptions import ValidationError  # Import this
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework import status
+from sslcommerz_lib import SSLCOMMERZ 
+
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     # queryset= Cart.objects.all()
@@ -115,3 +117,44 @@ class OrderViewSet(ModelViewSet):
         if self.request.user.is_staff:
             return Order.objects.prefetch_related('items__product').all()
         return Order.objects.prefetch_related('items__product').filter(user= self.request.user)
+    
+@api_view(['POST'])
+def initiate_payment(request):
+    user= request.user
+    amount= request.data.get("amount")
+    order_id= request.data.get("orderId")
+    num_items= request.data.get("numItems")
+    settings = { 
+                'store_id': 'phima680bc9617995d', 
+                'store_pass': 'phima680bc9617995d@ssl', 
+                'issandbox': True 
+                }
+    sslcz = SSLCOMMERZ(settings)
+    post_body = {}
+    post_body['total_amount'] = amount
+    post_body['currency'] = "BDT"
+    post_body['tran_id'] = f"trxn_{order_id}"
+    post_body['success_url'] = "http://localhost:5173/dashboard/payment/success/"
+    post_body['fail_url'] = "http://localhost:5173/dashboard/payment/success/"
+    post_body['cancel_url'] = "http://localhost:5173/dashboard/orders/"
+    post_body['emi_option'] = 0
+    post_body['cus_name'] = f"{user.first_name} {user.last_name}"
+    post_body['cus_email'] = user.email
+    post_body['cus_phone'] = user.phone_number
+    post_body['cus_add1'] = user.address
+    post_body['cus_city'] = "Dhaka"
+    post_body['cus_country'] = "Bangladesh"
+    post_body['shipping_method'] = "Courier"
+    post_body['multi_card_name'] = ""
+    post_body['num_of_item'] = num_items
+    post_body['product_name'] = "E-commerce Product"
+    post_body['product_category'] = "General"
+    post_body['product_profile'] = "general"
+
+
+    response = sslcz.createSession(post_body) # API response
+
+    # Need to redirect user to response['GatewayPageURL']
+    if response.get("status") == 'SUCCESS':
+        return Response({"payment_url": response["GatewayPageURL"]})
+    return Response({"error": "Payment initiation failed"}, status=status.HTTP_400_BAD_REQUEST)
