d789ec9866cf95bfeb68f536249eefc53ebebc18
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-04-26 21:34:03 +0600

    (bug_fixed)Productserializer images read_only:True

--- product/serializers.py
+++ product/serializers.py
@@ -50,7 +50,7 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    images= ProductImageSerializer(many= True)
+    images= ProductImageSerializer(many= True, read_only=True)
     class Meta:
         model= Product
         fields= ['id', 'name', 'description', 'price', 'stock', 'category', 'price_with_tax', 'images']
